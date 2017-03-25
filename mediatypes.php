0ef1573285580d8023ea0916259ce7d36ce465a0
Sein Coray <s3inlc@hashes.org>
2017-03-25 14:18:45 +0100

    added data loading for media type editing

--- mediatypes.php
+++ mediatypes.php
@@ -23,6 +23,22 @@ if(isset($_POST['action'])){
   $mediaTypeHandler->handle($_POST['action']);
 }
 
+if(isset($_GET['edit'])){
+  $mediaType = $FACTORIES::getMediaTypeFactory()->get($_GET['edit']);
+  if($mediaType != null){
+    $TEMPLATE = new Template("content/mediatypes/edit");
+    $OBJECTS['mediaType'] = $mediaType;
+    
+    $dir = TEMPLATES_PATH."/media/";
+    $templates = array();
+    foreach(scandir($dir) as $entry){
+      if(strpos($entry, ".template.html") !== false){
+        $templates[] = str_replace(".template.html", "", $entry);
+      }
+    }
+    $OBJECTS['templates'] = $templates;
+  }
+}
 
 $mediaTypes = $FACTORIES::getMediaTypeFactory()->filter(array());
 $OBJECTS['mediaTypes'] = $mediaTypes;

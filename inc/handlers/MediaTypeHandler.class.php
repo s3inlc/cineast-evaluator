a93b41a23707dec1560d90628a47234bbb264f55
Sein Coray <s3inlc@hashes.org>
2017-03-25 14:31:06 +0100

    added handling of mediaType update

--- inc/handlers/MediaTypeHandler.class.php
+++ inc/handlers/MediaTypeHandler.class.php
@@ -13,9 +13,30 @@ class MediaTypeHandler extends Handler {
    */
   public function handle($action) {
     switch ($action) {
+      case "updateType":
+        $this->updateType();
+        break;
       default:
         UI::addErrorMessage("Unknown action!");
         break;
     }
+  }
+  
+  private function updateType(){
+    global $FACTORIES;
+    
+    $typeName = htmlentities($_POST['typeName'], false, "UTF-8");
+    $template = htmlentities($_POST['template'], false, "UTF-8");
+    
+    $mediaType = $FACTORIES::getMediaTypeFactory()->get($_POST['mediaTypeId']);
+    if($mediaType == null){
+      UI::addErrorMessage("Invalid mediaType!");
+      return;
+    }
+    
+    $mediaType->setTemplate($template);
+    $mediaType->setTypeName($typeName);
+    $FACTORIES::getMediaTypeFactory()->update($mediaType);
+    UI::addSuccessMessage("MediaType was updated successfully!");
   }
 }

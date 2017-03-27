6265a0b57404cfc4da3d8ef85c2d46d06d5f3442
Sein Coray <sein.coray@gmail.com>
2017-03-27 18:25:18 +0200

    added similarity script placeholders

---
+++ inc/script/similarityCalculationCronjob.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: sein
+ * Date: 27.03.17
+ * Time: 18:24
+ */
+
+require_once(dirname(__FILE__) . "/../load.php");
+
+// TODO: get which script should be used
+// TODO: include the selected similarity calculator

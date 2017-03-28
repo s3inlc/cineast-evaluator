b38497b69c96d69b4eaaa40b9e1d48885fb2d84b
Sein Coray <sein.coray@gmail.com>
2017-03-28 15:35:00 +0200

    updated forwarding after login

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ require_once(dirname(__FILE__) . "/inc/load.php");
 /** @var array $OBJECTS */
 
 if (!isset($_POST['username']) || !isset($_POST['password'])) {
-  header("Location: index.php?err=1" . time());
+  header("Location: admin.php?err=1" . time());
   die();
 }
 
@@ -13,14 +13,14 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 if (strlen($username) == 0 || strlen($password) == 0) {
-  header("Location: index.php?err=2" . time());
+  header("Location: admin.php?err=2" . time());
   die();
 }
 
 $LOGIN->login($username, $password);
 if ($LOGIN->isLoggedin()) {
-  header("Location: index.php");
+  header("Location: admin.php");
   die();
 }
 
-header("Location: index.php?err=3" . time());
+header("Location: admin.php?err=3" . time());

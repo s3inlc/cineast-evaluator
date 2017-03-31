43b38cc42b73d01059cc9c1fe0aca058999b529b
Sein Coray <sein.coray@gmail.com>
2017-03-31 10:51:58 +0200

    added debug output for pool

--- inc/QuestionPool.class.php
+++ inc/QuestionPool.class.php
@@ -28,6 +28,7 @@ class QuestionPool {
     $startTime = microtime(true);
     
     $this->pool = array();
+    echo "Loading pool..<br>";
     
     // load tuples which are not answered from this user yet
     $qF1 = new QueryFilter(AnswerSession::PLAYER_ID, $answerSession->getPlayerId(), "=");
@@ -36,17 +37,19 @@ class QuestionPool {
     $answerSessions = $FACTORIES::getAnswerSessionFactory()->filter(array($FACTORIES::FILTER => array($qF1, $qF2, $qF3)));
     $answerSessionIds = array();
     foreach ($answerSessions as $a) {
-      if($a->getMicroworkerId() == null && $a->getPlayerId() == null && $a->getUserId() == null){
+      if ($a->getMicroworkerId() == null && $a->getPlayerId() == null && $a->getUserId() == null) {
         continue;
       }
       $answerSessionIds[] = $a->getId();
     }
     
-    if(sizeof($answerSessionIds)){
+    echo "User completet at least " . sizeof($answerSessionIds) . " sessions.<br>";
+    
+    if (sizeof($answerSessionIds)) {
       $qF = new ContainFilter(TwoCompareAnswer::ANSWER_SESSION_ID, $answerSessionIds);
       $twoAnswers = $FACTORIES::getTwoCompareAnswerFactory()->filter(array($FACTORIES::FILTER => $qF));
     }
-    else{
+    else {
       $twoAnswers = $FACTORIES::getTwoCompareAnswerFactory()->filter(array());
     }
     
@@ -55,30 +58,34 @@ class QuestionPool {
       $tupleIds[] = $twoAnswer->getResultTupleId();
     }
     
-    if(sizeof($tupleIds) > 0) {
+    if (sizeof($tupleIds) > 0) {
       $qF = new ContainFilter(ResultTuple::RESULT_TUPLE_ID, $tupleIds);
       $tuples = $FACTORIES::getResultTupleFactory()->filter(array($FACTORIES::FILTER => $qF));
     }
-    else{
+    else {
       $tuples = $FACTORIES::getResultTupleFactory()->filter(array());
     }
     
     // TODO: add ordering by priority, isClosed and progress
+    echo sizeof($tuples) . " tuples loaded<br>";
     
     $questions = array();
     $usedTuples = array();
     for ($i = 0; $i < SESSION_SIZE; $i++) {
-      if(sizeof($usedTuples) == sizeof($tuples)){
+      if (sizeof($usedTuples) == sizeof($tuples)) {
         break; // should only happen when there are not a lot of queries available
       }
       $tuple = $tuples[random_int(0, sizeof($tuples) - 1)];
-      while(in_array($tuple->getId(), $usedTuples)){
+      while (in_array($tuple->getId(), $usedTuples)) {
         $tuple = $tuples[random_int(0, sizeof($tuples) - 1)];
       }
+      echo "selecting tuple ".$tuple->getId()."...<br>";
       $mediaObjects = array($FACTORIES::getMediaObjectFactory()->get($tuple->getObjectId1()), $FACTORIES::getMediaObjectFactory()->get($tuple->getObjectId2()));
       $questions[] = new SessionQuestion(SessionQuestion::TYPE_COMPARE_TWO, $mediaObjects, array($tuple));
       $usedTuples[] = $tuple->getId();
     }
+    
+    echo "tuples selecting done!<br>";
     
     $endTime = microtime(true);
     $OBJECTS['loadTime'] = $endTime - $startTime;

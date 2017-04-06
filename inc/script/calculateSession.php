47d82230fe47638c43b6ecfa600415dace20fec4
Sein Coray <sein.coray@gmail.com>
2017-04-06 20:47:22 +0200

    added session calculator with all validators

--- inc/script/calculateSession.php
+++ inc/script/calculateSession.php
@@ -13,6 +13,10 @@ require_once(dirname(__FILE__) . "/../load.php");
 $session = $FACTORIES::getAnswerSessionFactory()->get($argv[1]);
 $validator = new MultivariantCrowdValidator();
 $currentValidity = $validator->validateFinished($session, 0);
+$validator = new PatternValidator();
+$currentValidity = $validator->validateFinished($session, $currentValidity);
+$validator = new TimeValidator();
+$currentValidity = $validator->validateFinished($session, $currentValidity);
 echo $currentValidity . "\n";
   
 

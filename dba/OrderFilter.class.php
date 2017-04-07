9f8ecfd34c40e7f936f94e59a668140d10c01381
Sein Coray <sein.coray@gmail.com>
2017-04-07 13:55:50 +0200

    fixed order filter to accept overrideFactory

--- dba/OrderFilter.class.php
+++ dba/OrderFilter.class.php
@@ -5,15 +5,21 @@ namespace DBA;
 class OrderFilter extends Order {
   private $by;
   private $type;
+  /** @var AbstractModelFactory */
+  private $overrideFactory;
   
-  function __construct($by, $type) {
+  function __construct($by, $type, $overrideFactory = null) {
     $this->by = $by;
     $this->type = $type;
+    $this->overrideFactory = $overrideFactory;
   }
   
   function getQueryString($table = "") {
     if ($table != "") {
       $table = $table . ".";
+    }
+    else if ($this->overrideFactory != null) {
+      $table = $this->overrideFactory->getModelTable() . ".";
     }
     return $table . $this->by . " " . $this->type;
   }

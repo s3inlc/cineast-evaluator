ab64025be82286af461335f9092f9f8c588c7691
Sein Coray <s3inlc@hashes.org>
2017-04-23 20:19:52 +0200

    added content loading for evaluated result tuples for a single query

--- evaluated.php
+++ evaluated.php
@@ -36,7 +36,7 @@ if (isset($_GET['view'])) {
     $mediaObject1 = $FACTORIES::getMediaObjectFactory()->get($resultTuple->getObjectId1());
     $mediaObject2 = $FACTORIES::getMediaObjectFactory()->get($resultTuple->getObjectId2());
     Util::prepare2CompareQuestion($mediaObject1, $mediaObject2, false);
-    
+
     $imageData = false;
     if ($resultTuple->getSigma() >= 0) {
       if ($resultTuple->getSigma() == 0) {
@@ -48,11 +48,25 @@ if (isset($_GET['view'])) {
     $OBJECTS['imageData'] = $imageData;
   }
 }
+else if(isset($_GET['queryId'])){
+    $TEMPLATE = new Template("content/evaluated/query");
+    $query = $FACTORIES::getQueryFactory()->get($_GET['queryId']);
+    if($query == null){
+        UI::addErrorMessage("Invalid query!");
+    }
+    else{
+        $qF1 = new QueryFilter(ResultTuple::IS_FINAL, "1", "=");
+        $qF2 = new QueryFilter(QueryResultTuple::QUERY_ID, $query->getId(), "=");
+        $jF = new JoinFilter($FACTORIES::getQueryResultTupleFactory(), ResultTuple::RESULT_TUPLE_ID, QueryResultTuple::RESULT_TUPLE_ID);
+        $joined = $FACTORIES::getResultTupleFactory()->filter(array($FACTORIES::FILTER => array($qF1, $qF2), $FACTORIES::JOIN => $jF));
+        $OBJECTS['tuples'] = $joined[$FACTORIES::getResultTupleFactory()->getModelName()];
+    }
+}
 else {
   $qF = new QueryFilter(ResultTuple::SIGMA, 0, ">=");
   $oF = new OrderFilter(ResultTuple::SIGMA, "ASC");
   $resultTuples = $FACTORIES::getResultTupleFactory()->filter(array($FACTORIES::FILTER => $qF, $FACTORIES::ORDER => $oF));
-  
+
   $numAnswers = new DataSet();
   $queries = new DataSet();
   foreach ($resultTuples as $resultTuple) {
@@ -60,14 +74,14 @@ else {
     $qF = new QueryFilter(TwoCompareAnswer::RESULT_TUPLE_ID, $resultTuple->getId(), "=");
     $count = $FACTORIES::getTwoCompareAnswerFactory()->countFilter(array($FACTORIES::FILTER => $qF));
     $numAnswers->addValue($resultTuple->getId(), $count);
-    
+
     // get queries for this tuple
     $qF = new QueryFilter(QueryResultTuple::RESULT_TUPLE_ID, $resultTuple->getId(), "=");
     $jF = new JoinFilter($FACTORIES::getQueryResultTupleFactory(), Query::QUERY_ID, QueryResultTuple::QUERY_ID);
     $joined = $FACTORIES::getQueryFactory()->filter(array($FACTORIES::FILTER => $qF, $FACTORIES::JOIN => $jF));
     $queries->addValue($resultTuple->getId(), $joined['Query']);
   }
-  
+
   $OBJECTS['resultTuples'] = $resultTuples;
   $OBJECTS['queries'] = $queries;
   $OBJECTS['numAnswers'] = $numAnswers;

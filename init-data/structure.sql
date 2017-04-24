0d174866fcb8904f8829bf0c2f84b68a99c3ef7a
Sein Coray <sein.coray@gmail.com>
2017-04-24 18:37:15 +0200

    increased length to handle forward IP results

--- init-data/structure.sql
+++ init-data/structure.sql
@@ -34,7 +34,7 @@ CREATE TABLE `AnswerSession` (
   `currentValidity` float NOT NULL,
   `isOpen` tinyint(11) NOT NULL,
   `timeOpened` int(11) NOT NULL,
-  `userAgentIp` varchar(20) COLLATE utf8_bin NOT NULL,
+  `userAgentIp` varchar(50) COLLATE utf8_bin NOT NULL,
   `userAgentHeader` text COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 

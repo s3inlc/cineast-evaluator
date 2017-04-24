4403ef319cef225a60f4ad931cb7260782972123
Sein Coray <sein.coray@gmail.com>
2017-04-24 15:22:36 +0200

    fixed url paths

--- oauth.php
+++ oauth.php
@@ -20,6 +20,6 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $files = $drive->files->listFiles(array())->getItems();
   echo json_encode($files);
 } else {
-  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/cineast-evaluator/oauth2callback.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }

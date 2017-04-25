7953ac6b98ba6be6f3eacac774cd3880879de9d4
Sein Coray <sein.coray@gmail.com>
2017-04-25 10:41:10 +0200

    fixed parsing

--- oauth.php
+++ oauth.php
@@ -18,8 +18,8 @@ $client->addScope(Google_Service_Oauth2::USERINFO_EMAIL);
 
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $client->setAccessToken($_SESSION['access_token']);
-  $info = file_get_contents("https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=".$client->getAccessToken());
-  print_r(json_encode($info, true));
+  $info = file_get_contents("https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=".$client->getAccessToken()['access_token']);
+  print_r($info);
 } else {
   $redirect_uri = 'oauth2callback.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));

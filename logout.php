c0ba1f8376a2287fb130fba76aeefdc439ac3f4d
Sein Coray <sein.coray@gmail.com>
2017-04-25 16:28:29 +0200

    removed bad code

--- logout.php
+++ logout.php
@@ -8,10 +8,6 @@ require_once(dirname(__FILE__) . "/inc/load.php");
 if(!$LOGIN->isLoggedin() && !$OAUTH->isLoggedin()){
   header("Location: index.php");
 }
-else if (!$LOGIN->isLoggedin()) {
-  header("Location: admin.php");
-  die();
-}
 
 if($OAUTH->isLoggedin()){
   $OAUTH->logout();

3dedfcc8e48f9a710b555cda60d184de2a2c51d0
Sein Coray <sein.coray@gmail.com>
2017-04-25 16:26:58 +0200

    added login/logout for oauth

--- logout.php
+++ logout.php
@@ -5,9 +5,17 @@ require_once(dirname(__FILE__) . "/inc/load.php");
 /** @var Login $LOGIN */
 /** @var array $OBJECTS */
 
-if (!$LOGIN->isLoggedin()) {
+if(!$LOGIN->isLoggedin() && !$OAUTH->isLoggedin()){
+  header("Location: index.php");
+}
+else if (!$LOGIN->isLoggedin()) {
   header("Location: admin.php");
   die();
+}
+
+if($OAUTH->isLoggedin()){
+  $OAUTH->logout();
+  header("Location: index.php");
 }
 
 $LOGIN->logout();

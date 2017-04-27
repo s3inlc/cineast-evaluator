e6a135c2f954939043ef9f69e343a2d7f6d0245d
Sein Coray <sein.coray@gmail.com>
2017-04-27 11:09:04 +0200

    added settings form

--- settings.php
+++ settings.php
@@ -7,8 +7,15 @@ use DBA\QueryFilter;
 
 require_once(dirname(__FILE__) . "/inc/load.php");
 
+if (!$OAUTH->isLoggedin()) {
+  header("Location: index.php?err=4" . time());
+  die();
+}
+
 $MENU->setActive("settings");
 $OBJECTS['pageTitle'] = "Settings";
 $TEMPLATE = new Template("content/settings");
+
+$OBJECTS['player'] = $OAUTH->getPlayer();
 
 echo $TEMPLATE->render($OBJECTS);

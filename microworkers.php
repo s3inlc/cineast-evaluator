0b2d083453db8f3d79b775ee4c5c92cdabe40db9
Sein Coray <sein.coray@gmail.com>
2017-05-18 12:41:07 +0200

    added batch view

--- microworkers.php
+++ microworkers.php
@@ -2,6 +2,9 @@
 
 /** @var $OBJECTS array */
 
+use DBA\Microworker;
+use DBA\QueryFilter;
+
 require_once(dirname(__FILE__) . "/inc/load.php");
 if (!$LOGIN->isLoggedin()) {
   header("Location: admin.php?err=4" . time());
@@ -19,6 +22,17 @@ if (isset($_POST['action'])) {
 
 if (isset($_GET['new'])) {
   $TEMPLATE = new Template("content/microworkers/new");
+}
+else if (isset($_GET['view'])) {
+  $batch = $FACTORIES::getMicroworkerBatchFactory()->get($_GET['view']);
+  if ($batch == null) {
+    UI::printError("ERROR", "Invalid Batch!");
+  }
+  $TEMPLATE = new Template("content/microworkers/detail");
+  $qF = new QueryFilter(Microworker::MICROWORKER_BATCH_ID, $batch->getId(), "=");
+  $microworkers = $FACTORIES::getMicroworkerFactory()->filter(array($FACTORIES::FILTER => $qF));
+  $OBJECTS['batch'] = $batch;
+  $OBJECTS['microworkers'] = $microworkers;
 }
 else {
   $TEMPLATE = new Template("content/microworkers/index");

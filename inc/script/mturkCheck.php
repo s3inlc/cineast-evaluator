23f0439f1c7d67aabc31b1bc0fbabac398a065c5
Sein Coray <sein.coray@gmail.com>
2017-05-19 11:19:05 +0200

    updated credentials using

--- inc/script/mturkCheck.php
+++ inc/script/mturkCheck.php
@@ -9,7 +9,14 @@
 require_once(dirname(__FILE__) . "/../load.php");
 
 // conntect to API
-$client = new \Aws\MTurk\MTurkClient(array("version" => "latest", "region" => "us-east-1", "endpoint_url" => "https://mturk-requester-sandbox.us-east-1.amazonaws.com"));
+$credentials = json_decode(file_get_contents(dirname(__FILE__) . "/../mturk_api_key.json"), true);
+$client = new \Aws\MTurk\MTurkClient(array(
+  "version" => "latest",
+  "region" => "us-east-1",
+  "endpoint_url" => "https://mturk-requester-sandbox.us-east-1.amazonaws.com",
+  "credentials" => $credentials
+)
+);
 
 $hits = $client->listHITs();
 

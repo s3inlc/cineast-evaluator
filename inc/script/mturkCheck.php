50f6cc5b890eed8ce962ba15b9fdaed372ebf96c
Sein Coray <sein.coray@gmail.com>
2017-05-19 12:08:39 +0200

    added checking of token

--- inc/script/mturkCheck.php
+++ inc/script/mturkCheck.php
@@ -6,6 +6,9 @@
  * Time: 11:04
  */
 
+use DBA\Microworker;
+use DBA\QueryFilter;
+
 require_once(dirname(__FILE__) . "/../load.php");
 
 // conntect to API
@@ -17,6 +20,8 @@ $client = new \Aws\MTurk\MTurkClient(array(
     "credentials" => $credentials
   )
 );
+
+$client->listAs
 
 $nextToken = null;
 do {
@@ -30,7 +35,28 @@ do {
     $nextToken = $result->toArray()['NextToken'];
   }
   foreach ($hits as $hit) {
-    echo "Processing HIT " . $hit['HITId'] . " with status: " . $hit['HITStatus'] . ", expiration: " . $hit['Expiration'] . "... ";
+    echo "Processing HIT " . $hit['HITId'] . " with status: " . $hit['HITStatus'] . "... ";
+    
+    $matches = array();
+    preg_match('/\&token\=([a-zA-Z0-9]*?)\"/', $hit['Question'], $matches);
+    $token = $matches[1];
+    echo "\n  check token $token... ";
+    
+    $qF = new QueryFilter(Microworker::TOKEN, $token, "=");
+    $microworker = $FACTORIES::getMicroworkerFactory()->filter(array($FACTORIES::FILTER => $qF), true);
+    if ($microworker->getTimeClosed() == 0) {
+      echo "Not started yet\n";
+      continue;
+    }
+    else if ($microworker->getIsConfirmed() == 1) {
+      echo "Already confirmed\n";
+      continue;
+    }
+    else if ($microworker->getIsLocked() == 1) {
+      echo "Is locked\n";
+      continue;
+    }
+    
     $result = $client->listAssignmentsForHIT(array(
         "HITId" => $hit['HITId'],
         "AssignmentStatuses" => array("Submitted")
@@ -43,6 +69,7 @@ do {
     else {
       echo " " . sizeof($assignments) . " Assignments:\n";
       foreach ($assignments as $assignment) {
+        $matches = array();
         preg_match('/\<FreeText\>(.*?)\<\/FreeText\>/', $assignment['Answer'], $matches);
         $answer = $matches[1];
         echo "  " . $assignment['AssignmentId'] . " by " . $assignment['WorkerId'] . ": " . $answer . "\n";

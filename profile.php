acce33d5395980ddc62b0b4191e3d92fee96cae7
Sein Coray <sein.coray@gmail.com>
2017-05-21 19:07:55 +0200

    added profile stats

--- profile.php
+++ profile.php
@@ -1,6 +1,10 @@
 <?php
 
 /** @var $OBJECTS array */
+use DBA\Game;
+use DBA\OrderFilter;
+use DBA\QueryFilter;
+
 /** @var $OAUTH OAuthLogin */
 
 require_once(dirname(__FILE__) . "/inc/load.php");
@@ -26,4 +30,70 @@ else {
 }
 $OBJECTS['player'] = $player;
 
+$qF = new QueryFilter(Game::PLAYER_ID, $player->getId(), "=");
+$oF = new OrderFilter(Game::FINISHED_TIME, "ASC");
+$games = $FACTORIES::getGameFactory()->filter(array($FACTORIES::FILTER => $qF, $FACTORIES::ORDER => $oF));
+$totalScore = 0;
+$playedGames = 0;
+/** @var $latestGame Game */
+$latestGame = null;
+/** @var $highestBaseGame Game */
+$highestBaseGame = null;
+/** @var $highestFullGame Game */
+$highestFullGame = null;
+foreach ($games as $game) {
+  if ($highestBaseGame == null || $highestBaseGame->getGameScore() < $game->getGameScore()) {
+    $highestBaseGame = $game;
+  }
+  if ($highestFullGame == null || $highestFullGame->getFullScore() < $game->getFullScore()) {
+    $highestFullGame = $game;
+  }
+  $totalScore += $game->getFullScore();
+  $playedGames++;
+  $latestGame = $game;
+}
+
+if ($playedGames > 0) {
+  $averageScore = Util::number(floor($totalScore / $playedGames));
+  $highestBase = Util::number($highestBaseGame->getGameScore());
+  $highestFull = Util::number($highestFullGame->getFullScore());
+  $oF1 = new OrderFilter(Game::GAME_SCORE, "DESC");
+  $oF2 = new OrderFilter(Game::FINISHED_TIME, "DESC");
+  $games = $FACTORIES::getGameFactory()->filter(array($FACTORIES::ORDER => $oF));
+  $baseRank = 0;
+  $fullRank = 0;
+  $count = 1;
+  foreach ($games as $game) {
+    if ($game->getId() == $highestBaseGame->getId()) {
+      $baseRank = $count;
+    }
+    if ($game->getId() == $highestFullGame->getId()) {
+      $fullRank = $count;
+    }
+    $count++;
+  }
+}
+else {
+  $averageScore = "N/A";
+  $baseRank = "--";
+  $fullRank = "--";
+  $highestFull = "N/A";
+  $highestBase = "N/A";
+}
+
+$OBJECTS['highestBase'] = $highestBase;
+$OBJECTS['highestFull'] = $highestFull;
+$OBJECTS['playedGames'] = $playedGames;
+$OBJECTS['averageScore'] = $averageScore;
+$OBJECTS['baseRank'] = $baseRank;
+$OBJECTS['fullRank'] = $fullRank;
+$OBJECTS['latestGame'] = $latestGame;
+
 echo $TEMPLATE->render($OBJECTS);
+
+
+
+
+
+
+

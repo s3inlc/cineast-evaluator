b00aeff39d815189d5811f22abd0503f1d3fa692
Sein Coray <sein.coray@gmail.com>
2017-05-22 16:50:34 +0200

    updated code to get

--- oauth2callback.php
+++ oauth2callback.php
@@ -110,7 +110,7 @@ else if ($provider == OAuthLogin::TYPE_FACEBOOK) {
   $_SESSION['accessToken'] = (string)$accessToken;
   try {
     // Returns a `Facebook\FacebookResponse` object
-    $response = $fb->get('/me?fields=id,name', $accessToken);
+    $response = $fb->get('/me?fields=id,name,email', $accessToken);
   }
   catch (Facebook\Exceptions\FacebookResponseException $e) {
     echo 'Graph returned an error: ' . $e->getMessage();

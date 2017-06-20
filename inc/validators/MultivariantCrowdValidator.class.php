0b2066caeef20af7a9e47f34312966d301d12997
Sein Coray <sein.coray@gmail.com>
2017-06-20 12:09:54 +0200

    constant factor test

--- inc/validators/MultivariantCrowdValidator.class.php
+++ inc/validators/MultivariantCrowdValidator.class.php
@@ -79,7 +79,7 @@ class MultivariantCrowdValidator extends Validator {
             $prob *= 2;
             echo "Increased 2 -> ";
           }
-          $factor *= 0.8 + (min($prob, 1));
+          $factor *= 0.6 + (min($prob, 1));
           echo "Probability on " . $resultSets[$i]->getId() . ": " . $prob . ":fac:" . $factor . "\n";
         }
         $sum += $prob;

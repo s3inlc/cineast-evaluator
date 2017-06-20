d762d0403849d7b68f7f7ea8da7760202e5dd533
Sein Coray <sein.coray@gmail.com>
2017-06-20 11:16:02 +0200

    added handling of validation on cleanup added time shifter

--- inc/script/validityMassTest.php
+++ inc/script/validityMassTest.php
@@ -9,6 +9,7 @@
 use DBA\AnswerSession;
 use DBA\ContainFilter;
 use DBA\TwoCompareAnswer;
+use DBA\Validation;
 
 require_once(dirname(__FILE__) . "/../load.php");
 
@@ -19,13 +20,15 @@ for ($i = 0; $i < $numTests; $i++) {
   $answerSession = new AnswerSession(0, null, null, null, 0, 0, time(), "999.999.999.999", "EMPTY");
   $answerSession = $FACTORIES::getAnswerSessionFactory()->save($answerSession);
   $answerSessions[] = $answerSession->getId();
+  $incrementor = 0;
   
   $questions = $pool->getNextQuestionBlock($answerSession);
   $answers = array();
   foreach ($questions as $question) {
     $answer = mt_rand(0, 3); // get a random answer
-    $twoCompareAnswer = new TwoCompareAnswer(0, time(), $question->getResultTuples()[0]->getId(), $answer, $answerSession->getId());
+    $twoCompareAnswer = new TwoCompareAnswer(0, time() + $incrementor, $question->getResultTuples()[0]->getId(), $answer, $answerSession->getId());
     $answers[] = $twoCompareAnswer;
+    $incrementor += 3;
   }
   $FACTORIES::getTwoCompareAnswerFactory()->massSave($answers);
   
@@ -40,6 +43,9 @@ for ($i = 0; $i < $numTests; $i++) {
 }
 
 // cleanup
+$qF = new ContainFilter(Validation::ANSWER_SESSION_ID, $answerSessions);
+$FACTORIES::getValidationFactory()->massDeletion(array($FACTORIES::FILTER => $qF));
+
 $qF = new ContainFilter(TwoCompareAnswer::ANSWER_SESSION_ID, $answerSessions);
 $FACTORIES::getTwoCompareAnswerFactory()->massDeletion(array($FACTORIES::FILTER => $qF));
 

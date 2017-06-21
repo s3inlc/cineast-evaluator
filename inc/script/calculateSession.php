897a8ac3cdb7773a207f54156a4d8e3d6ed213f8
Sein Coray <sein.coray@gmail.com>
2017-06-21 14:10:05 +0200

    added debug output

--- inc/script/calculateSession.php
+++ inc/script/calculateSession.php
@@ -13,8 +13,10 @@ $session = $FACTORIES::getAnswerSessionFactory()->get($argv[1]);
 $validator = new MultivariantCrowdValidator();
 $currentValidity = $validator->validateFinished($session, 0, true);
 $validator = new PatternValidator();
+echo $currentValidity . "\n";
 $currentValidity = $validator->validateFinished($session, $currentValidity);
 $validator = new TimeValidator();
+echo $currentValidity . "\n";
 $currentValidity = $validator->validateFinished($session, $currentValidity);
 echo $currentValidity . "\n";
 

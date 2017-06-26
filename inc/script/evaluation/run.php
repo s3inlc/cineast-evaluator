1be3144dd6d9ccc60e5c67cf02d498bda00c7dea
Sein Coray <sein.coray@gmail.com>
2017-06-26 13:36:53 +0200

    added first evaluation files

--- inc/script/evaluation/run.php
+++ inc/script/evaluation/run.php
@@ -6,8 +6,59 @@
  * Time: 09:56
  */
 
+use DBA\Game;
+use DBA\Microworker;
+use DBA\QueryFilter;
+use DBA\ResultTuple;
+
 require_once(dirname(__FILE__) . "/../../load.php");
 
 $FACTORIES::getAnswerSessionFactory()->startTransation();
 
 // determine some global numbers
+
+$totalQueries = $FACTORIES::getQueryFactory()->countFilter(array());
+$totalTuples = $FACTORIES::getResultTupleFactory()->countFilter(array());
+
+$qF = new QueryFilter(ResultTuple::IS_FINAL, 1, "=");
+$fullyEvaluatedTuples = $FACTORIES::getResultTupleFactory()->countFilter(array($FACTORIES::FILTER => $qF));
+
+$totalPlayers = $FACTORIES::getPlayerFactory()->countFilter(array());
+
+$qF = new QueryFilter(Microworker::IS_CONFIRMED, 1, "=");
+$totalMicroworkers = $FACTORIES::getMicroworkerFactory()->countFilter(array($FACTORIES::FILTER => $qF));
+
+$totalGames = $FACTORIES::getGameFactory()->countFilter(array());
+
+
+// days of playing by players and games per player
+$players = $FACTORIES::getPlayerFactory()->filter(array());
+$daysOfPlaying = array();
+$gamesPlayed = array();
+foreach ($players as $player) {
+  $qF = new QueryFilter(Game::PLAYER_ID, $player->getId(), "=");
+  $games = $FACTORIES::getGameFactory()->filter(array($FACTORIES::FILTER => $qF));
+  $gamesPlayed[] = array("playerId" => $player->getId(), "gamesPlayed" => sizeof($games));
+}
+saveCSV($gamesPlayed, dirname(__FILE__) . "/output/gamesPlayed.csv");
+
+
+/**
+ * @param $elements array
+ * @param $path
+ */
+function saveCSV($elements, $path) {
+  if (sizeof($elements) == 0) {
+    return;
+  }
+  $header = array();
+  $arr = $elements[0];
+  foreach ($arr as $key => $val) {
+    $header[] = $key;
+  }
+  $output = implode(",", $header) . "\n";
+  foreach ($elements as $element) {
+    $output .= implode(",", $element);
+  }
+  file_put_contents($path, $output);
+}

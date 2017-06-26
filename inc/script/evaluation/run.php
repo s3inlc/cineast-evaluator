c46848918de169ff68620116e337b42fa80e5f70
Sein Coray <sein.coray@gmail.com>
2017-06-26 13:51:11 +0200

    fixed case when user only played one game

--- inc/script/evaluation/run.php
+++ inc/script/evaluation/run.php
@@ -59,11 +59,15 @@ foreach ($players as $player) {
     else {
       $days[$date]++;
     }
-    
-    $daysOfPlaying[] = array("playerId" => $player->getId(), "days" => sizeof($days));
-    $gamesPerDay[] = array("playerId" => $player->getId(), "games" => round(sizeof($games) / sizeof($days), 2));
-    $gamesPerDayOverall[] = array("playerId" => $player->getId(), "games" => round(sizeof($games) / (($range[1] - $range[0]) / 3600 / 24), 2));
   }
+  
+  if ($range[0] == $range[1]) {
+    $range[1] = $range[0] + 3600 * 24;
+  }
+  
+  $daysOfPlaying[] = array("playerId" => $player->getId(), "days" => sizeof($days));
+  $gamesPerDay[] = array("playerId" => $player->getId(), "games" => round(sizeof($games) / sizeof($days), 2));
+  $gamesPerDayOverall[] = array("playerId" => $player->getId(), "games" => round(sizeof($games) / (($range[1] - $range[0]) / 3600 / 24), 2));
 }
 saveCSV($gamesPlayed, dirname(__FILE__) . "/output/gamesPlayed.csv");
 saveCSV($daysOfPlaying, dirname(__FILE__) . "/output/daysOfPlaying.csv");
